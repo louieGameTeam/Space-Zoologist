d19e3c0725e3b1b72b201708fa0968e13e494d4f
Caleb Bolton <calbolt22@gmail.com>
2020-02-26 12:47:48 -0800

    fixing things 2

---
+++ Space-Zoologist/Assets/Scripts/Population.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+    private Species _species;
+    public Species species { get; set; }
+
+    private void Awake()
+    {
+        gameObject.AddComponent<SpriteRenderer>();
+        
+    }
+
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = species.sprite;
+    }
+}
---
+++ Space-Zoologist/Assets/Scripts/PopulationManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour
+{
+    // Temporary
+    [SerializeField] private Species speciesToMake = default;
+
+    private List<Population> populations = default;
+
+    private float interval = 5.0f;
+    private float elapsed = 0.0f;
+    private void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= interval)
+        {
+            elapsed = 0.0f;
+            CreatePopulation(speciesToMake);
+        }
+    }
+
+    public void CreatePopulation(Species species)
+    {
+        GameObject gameObject = Instantiate(new GameObject(), this.transform);
+        gameObject.AddComponent<Population>().species = species;
+    }
+
+}
