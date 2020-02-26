a565e5f6d52b7833d138d65fa37a69c81b2b459e
Caleb Bolton <calbolt22@gmail.com>
2020-02-26 12:46:16 -0800

    fixing things 1

--- Space-Zoologist/Assets/Scripts/Types.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum NeedCondition { Bad, Neutral, Good }
-public enum SpeciesType { Strot, Madle, Jetter }
-public enum FoodSourceType { SpaceMaple, BerryTree, LeafyBush }
