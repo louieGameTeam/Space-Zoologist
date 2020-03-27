884e4e0bfcd9ea8bf0929c5c5b0961d6a90d576f
CalBolt <calbolt22@gmail.com>
2020-03-27 13:20:43 -0700

    Refactored SpeciesNeed and added a tester for it

--- Space-Zoologist/Assets/Scripts/SpeciesNeed.cs
+++ Space-Zoologist/Assets/Scripts/SpeciesNeed.cs
@@ -27,26 +27,15 @@ public class SpeciesNeed : ScriptableObject
         // If there is only one condition, return it.
         if (conditions.Count == 1) return conditions[0];
 
-        NeedCondition needCondition = NeedCondition.Neutral;
-
-        // Below or above threshold.
-        if (value <= this.thresholds[0])
+        for (var i = 0; i < this.thresholds.Count; i++)
         {
-            needCondition = this.conditions[0];
-        }
-        else if (value >= this.thresholds[this.thresholds.Count - 1])
-        {
-            needCondition = this.conditions[this.thresholds.Count - 1];
-        }
-        // Between threshold values.
-        for (int i = 1; i < this.thresholds.Count - 2; i++)
-        {
-            if ((value >= this.thresholds[i]) && (value < this.thresholds[i + 1]))
+            if (value < this.thresholds[i])
             {
-                needCondition = this.conditions[i];
+                return this.conditions[i];
             }
         }
-        return needCondition;
+
+        return this.conditions[this.thresholds.Count];
     }
 
     public void OnValidate()
