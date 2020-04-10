381f77fb43aeb03cbc295dd6741e51e2e1e44512
Souhi Ou <vxfwang@ucdavis.edu>
2020-04-10 08:40:29 -0700

    Added small offset to grass, Liquid background layer fix

--- Space-Zoologist/Assets/Scripts/Grid/Tiles/GrassColoringMethod.cs
+++ Space-Zoologist/Assets/Scripts/Grid/Tiles/GrassColoringMethod.cs
@@ -59,7 +59,7 @@ public class GrassColoringMethod : ColoringMethod
     }
     private float ColorGradient (float distance)
     {
-        float ratio = (1 - Mathf.Sqrt(distance / affectedRange));
+        float ratio = (1 - Mathf.Sqrt(distance / affectedRange) - 0.2f);
         if (ratio < 0)
         {
             ratio = 0;
