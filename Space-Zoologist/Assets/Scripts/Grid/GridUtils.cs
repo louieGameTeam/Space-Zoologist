e3e19dc846aa22b9032895bba4e1d59b4ec7989c
Souhi Ou <vxfwang@ucdavis.edu>
2020-07-14 09:38:25 -0700

    Plot prefab update, added walls

--- Space-Zoologist/Assets/Scripts/Grid/GridUtils.cs
+++ Space-Zoologist/Assets/Scripts/Grid/GridUtils.cs
@@ -14,7 +14,8 @@ public class GridUtils
         LiquidSurface,
         LiquidTexture,
         Grass,
-        Edge
+        Edge,
+        Wall
     }
     private static float[,] defautInterpolationArray = { { 1f, 1f, 1f }, { 1f, 1f, 0f }, { 1f, 0f, 0f }, { 1f, 0.565f, 0f }, { 0.153f, 0.443f, 0.698f }, { 0f, 0.561f, 0.357f }, { 0.439f, 0.212f, 0.588f }, { 0f, 0f, 0f } }; // RYB interpolation cube based on https://ieeexplore.ieee.org/document/5673980
     // public static readonly float[,] interpolationArray = new float[,] { { 1f, 1f, 1f }, { 1f, 1f, 0f }, { 1f, 0f, 0f }, { 1f, 0.5f, 0f }, { 0.163f, 0.373f, 0.6f }, { 0f, 0.66f, 0.2f }, { 0.5f, 0f, 0.5f }, { 0.2f, 0.094f, 0f } }; // Alternative based on https://ieeexplore.ieee.org/document/1382898
