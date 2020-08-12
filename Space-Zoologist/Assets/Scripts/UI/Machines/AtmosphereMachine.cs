e0b5140fb6fb6d81b2a5c54e37859bd270acaa98
Travis Garcia <tgar@ucdavis.edu>
2020-08-12 16:40:11 -0700

    added atmosphere interface

---
+++ Space-Zoologist/Assets/Scripts/UI/Machines/AtmosphereMachine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtmosphereMachine : MonoBehaviour
+{
+    [SerializeField] List<AtmosphereMachineValues> AtmosphereMachineValues = default;
+
+    public void Start()
+    {
+        this.UpdateStartingValues();
+    }
+
+    public void UpdateStartingValues()
+    {
+        foreach (AtmosphereMachineValues atm in this.AtmosphereMachineValues)
+        {
+            atm.UpdateStartingValue();
+        }
+    }
+
+    public void RevertStartingValues()
+    {
+        foreach (AtmosphereMachineValues atm in this.AtmosphereMachineValues)
+        {
+            atm.RevertStartingValue();
+        }
+    }
+}
---
+++ Space-Zoologist/Assets/Scripts/UI/Machines/AtmosphereMachineValues.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtmosphereMachineValues : MonoBehaviour
+{
+    [SerializeField] Text MachineValue = default;
+    int StartingValue = default;
+
+    public void UpdateStartingValue()
+    {
+        this.StartingValue = Int32.Parse(this.MachineValue.text);
+    }
+
+    public void RevertStartingValue()
+    {
+        this.MachineValue.text = this.StartingValue.ToString();
+    }
+
+    public void IncrementValue()
+    {
+        int value = Int32.Parse(this.MachineValue.text);
+        if (value < 100)
+        {
+            value++;
+            this.MachineValue.text = value.ToString();
+        }
+    }
+
+    public void DecrementValue()
+    {
+        int value = Int32.Parse(this.MachineValue.text);
+        if (value > 0)
+        {
+            value--;
+            this.MachineValue.text = value.ToString();
+        }
+    }
+}
