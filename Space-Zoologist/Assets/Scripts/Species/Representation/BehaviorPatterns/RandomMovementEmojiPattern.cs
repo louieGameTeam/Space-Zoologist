fb73490db0a3ddb2eda4bf793d269143d1c8f928
Akasteria <vxfwang@ucdavis.edu>
2020-08-24 11:20:20 -0700

    update

--- Space-Zoologist/Assets/Scripts/Species/Representation/BehaviorPatterns/RandomMovementEmojiPattern.cs
+++ Space-Zoologist/Assets/Scripts/Species/Representation/BehaviorPatterns/RandomMovementEmojiPattern.cs
@@ -6,7 +6,7 @@ public class RandomMovementEmojiPattern : BehaviorPattern
 {
     [SerializeField] Sprite Emoji = default;
     private System.Random random = new System.Random();
-    protected override void EnterPattern(GameObject gameObject)
+    protected override void EnterPattern(GameObject gameObject, AnimalData animalData)
     {
         int locationIndex = this.random.Next(0, AnimalsToAnimalData[gameObject].animal.PopulationInfo.AccessibleLocations.Count);
         Vector3Int end = AnimalsToAnimalData[gameObject].animal.PopulationInfo.AccessibleLocations[locationIndex];
