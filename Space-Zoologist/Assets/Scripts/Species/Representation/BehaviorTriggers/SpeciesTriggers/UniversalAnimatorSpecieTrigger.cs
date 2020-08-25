9db7660ff433f8781db991b467ee61f26a036b41
Akasteria <vxfwang@ucdavis.edu>
2020-08-25 12:10:50 -0700

    update Liquid body methods

---
+++ Space-Zoologist/Assets/Scripts/Species/Representation/BehaviorTriggers/SpeciesTriggers/UniversalAnimatorSpecieTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "SleepBehavior", menuName = "SpeciesBehavior/UniversalAnimatorSpecieTrigger")]
+public class UniversalAnimatorSpecieTrigger : SpecieBehaviorTrigger
+{
+    protected override List<GameObject> AnimalSelection(Dictionary<Availability, List<GameObject>> avalabilityToAnimals)
+    {
+        return BehaviorUtils.SelectAnimals(1, avalabilityToAnimals, SelectionType.All);
+    }
+}
