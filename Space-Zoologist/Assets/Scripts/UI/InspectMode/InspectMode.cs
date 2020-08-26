b871990fd320117cfce87345342450c421749125
Ethan Chiang <ethanyichiang@gmail.com>
2020-08-26 10:52:17 +0900

    added inspector UI

---
+++ Space-Zoologist/Assets/Scripts/UI/InspectMode/InspectMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InspectMode : MonoBehaviour
+{
+    private bool isInInspectorMode = false;
+
+    [SerializeField]
+    private GameObject HUD = null;
+    [SerializeField]
+    private GameObject inspectorWindow = null;
+
+    public void TuggleInspectMode()
+    {
+        this.isInInspectorMode = !isInInspectorMode;
+        this.inspectorWindow.SetActive(isInInspectorMode);
+        this.HUD.SetActive(!isInInspectorMode);
+        //Debug.Log($"Inspector mode is {this.isInInspectorMode}");
+    }
+}
