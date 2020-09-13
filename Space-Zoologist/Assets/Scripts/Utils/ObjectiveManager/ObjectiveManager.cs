35ff1951b54cfcc4a47fc22a25ca947dfb1b362d
Ethan Chiang <ethanyichiang@gmail.com>
2020-09-13 20:50:37 +0900

    added toggle function

--- Space-Zoologist/Assets/Scripts/Utils/ObjectiveManager/ObjectiveManager.cs
+++ Space-Zoologist/Assets/Scripts/Utils/ObjectiveManager/ObjectiveManager.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveManager : MonoBehaviour
 {
     [Expandable] public LevelObjectives levelObjectives = default;
 
+    private bool isOpen = false;
 
+    // To access the populations 
+    [SerializeField] private PopulationManager populationManager = default;
+    // To access the player balance
+    [SerializeField] private PlayerBalance playerBalance = default;
+    // Objective panel
+    [SerializeField] private GameObject objectivePanel = default;
+
+    public void ToggleObjectivePanel()
+    {
+        this.isOpen = !this.isOpen;
+        this.objectivePanel.SetActive(this.isOpen);
+    }
 }
