a7587fabc50ec76439b25ea478a33bf3a870ef0b
tg1230 <32658968+tg1230@users.noreply.github.com>
2020-09-17 10:58:45 -0700

    lineanimalsup hopefully not breaking things now

--- Space-Zoologist/Assets/AnimalRepresentation/Patterns/Scripts/TwoAnimalsLineUp.cs
+++ Space-Zoologist/Assets/AnimalRepresentation/Patterns/Scripts/TwoAnimalsLineUp.cs
@@ -41,14 +41,14 @@ public class TwoAnimalsLineUp : BehaviorPattern
         return isLinedUp;
     }
 
-    // protected override bool IsAlternativeConditionSatisfied(GameObject animal, AnimalData animalData)
-    // {
-    //     float distanceBetweenAnimals = animalData.collaboratingAnimals[0].transform.position.x - animal.transform.position.x;
-    //     if (!(distanceBetweenAnimals >= -3f && distanceBetweenAnimals <=3f))
-    //     {
-    //         Debug.Log("Couldn't line animals up, exiting early");
-    //         return true;
-    //     }
-    //     return false;
-    // }
+    protected override bool IsAlternativeConditionSatisfied(GameObject animal, AnimalData animalData)
+    {
+        float distanceBetweenAnimals = animalData.collaboratingAnimals[0].transform.position.x - animal.transform.position.x;
+        if (!(distanceBetweenAnimals >= -3f && distanceBetweenAnimals <=3f))
+        {
+            Debug.Log("Couldn't line animals up, exiting early");
+            return true;
+        }
+        return false;
+    }
 }
