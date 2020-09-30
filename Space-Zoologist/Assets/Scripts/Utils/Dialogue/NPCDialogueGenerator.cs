23a9f6553bdfdb5b5e40e5a5e3b371417c617c9f
Ethan Chiang <ethanyichiang@gmail.com>
2020-09-30 06:20:34 -0700

    added data structure to store dialogues

--- Space-Zoologist/Assets/Scripts/Utils/Dialogue/NPCDialogueGenerator.cs
+++ Space-Zoologist/Assets/Scripts/Utils/Dialogue/NPCDialogueGenerator.cs
@@ -9,6 +9,39 @@ using TMPro;
 /// </summary>
 public class NPCDialogueGenerator : MonoBehaviour
 {
+    public enum DialogueOutputOption { Good, Bad, Hint };
+
+    /// <summary>
+    /// Data structure to store parsed dialogues from sheet
+    /// </summary>
+    public class DialogueOption
+    {
+        public string Name => this.name;
+
+        private string name;
+        private List<string> goodDialgoues;
+        private List<string> badDialogues;
+        private List<string> hintDialogues;
+
+        public string GetDialogue(DialogueOutputOption option)
+        {
+            switch(option)
+            {
+                case DialogueOutputOption.Good:
+                    break;
+                case DialogueOutputOption.Bad:
+                    break;
+                case DialogueOutputOption.Hint:
+                    break;
+                default:
+                    break;
+            }
+
+            return $"{option} is not valid!";
+        }
+    }
+
+
     [SerializeField] private TextMeshProUGUI dialogueTextMeshPro = default;
     [SerializeField] private ObjectiveManager objectiveManager = default;
 
@@ -19,7 +52,7 @@ public class NPCDialogueGenerator : MonoBehaviour
 
     public void NPCTalksAboutPopulationStatus()
     {
-        this.dialogueTextMeshPro.text = $"Displaying something about the population";
+        this.dialogueTextMeshPro.text = $"You can select the populaion you want to ";
     }
 
     public void NPCTalksAboutJournalProgress()
