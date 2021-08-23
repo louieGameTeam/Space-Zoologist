84f9f368aa104f18149a896f9f39b61def9dc33a
Codey Huntting <creativecodey@gmail.com>
2021-08-23 11:02:12 -0700

    Found the right solution for the bookmark button

--- Space-Zoologist/Assets/Scripts/Notebook/UI/NotebookBookmarkNavigateButton.cs
+++ Space-Zoologist/Assets/Scripts/Notebook/UI/NotebookBookmarkNavigateButton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 using TMPro;
 
@@ -19,6 +20,24 @@ public class NotebookBookmarkNavigateButton : NotebookUIChild
 
     // Bookmark represented by this button
     private NotebookBookmark bookmark;
+    // The game object previously selected on the event system
+    private GameObject previousSelectedGameObject;
+    // The number of clicks this button has received while selected
+    private int clicksWhileSelected = 0;
+
+    private void Update()
+    {
+        GameObject currentSelectedGameObject = EventSystem.current.currentSelectedGameObject;
+
+        // If the current selected game object just changed, and it used to be the navigate button, reset clicks while selected
+        if(previousSelectedGameObject != currentSelectedGameObject && previousSelectedGameObject == navigateButton.gameObject)
+        {
+            clicksWhileSelected = 0;
+        }
+
+        // Set the previous selected object at the end of the frame
+        previousSelectedGameObject = currentSelectedGameObject;
+    }
 
     public void Setup(NotebookBookmark bookmark)
     {
@@ -33,7 +52,10 @@ public class NotebookBookmarkNavigateButton : NotebookUIChild
     // On click select the correct tab, and setup the category picker
     private void OnNavigateButtonClicked()
     {
-        UIParent.NavigateToBookmark(bookmark);
+        clicksWhileSelected++;
+
+        // If this is the second click while selected then navigate to the bookmark
+        if(clicksWhileSelected >= 2) UIParent.NavigateToBookmark(bookmark);
     }
 
     private void OnDeleteToggleChanged(bool isOn)
