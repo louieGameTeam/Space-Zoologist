d007d1a28b91c6db9466d19d4038f7726796853f
mandarker <springnguyen98@gmail.com>
2021-09-06 14:39:14 -0700

    Add painting support

--- Space-Zoologist/Assets/Scripts/Notebook/UI/DrawingCanvas.cs
+++ Space-Zoologist/Assets/Scripts/Notebook/UI/DrawingCanvas.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DrawingCanvas : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -56,6 +57,14 @@ public class DrawingCanvas : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     [SerializeField]
     [Tooltip("Current weight of the lines to draw")]
     private StrokeWeight currentWeight;
+
+    [SerializeField]
+    private int texturePixelSize;
+    [SerializeField, Range(0, 1)]
+    private float canvasBackgroundTransparency;
+    private Texture2D DrawingTexture;
+    private Material DrawingMaterial;
+    private Color defaultColor;
     #endregion
 
     #region Public Methods
@@ -71,6 +80,108 @@ public class DrawingCanvas : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     public void OnEndDrag(PointerEventData data)
     {
         cursor.FinishDrawing();
+    }
+    #endregion
+
+    #region Monobehaviour Callbacks
+    private void Start()
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        DrawingTexture = new Texture2D((int)rectTransform.rect.width / texturePixelSize, (int)rectTransform.rect.height / texturePixelSize);
+        defaultColor = Color.white;
+        defaultColor.a = canvasBackgroundTransparency;
+        for (int i = 0; i < DrawingTexture.width; ++i)
+        {
+            for (int j = 0; j < DrawingTexture.height; ++j)
+            {
+                DrawingTexture.SetPixel(i, j, defaultColor);
+            }
+        }
+        DrawingTexture.Apply();
+        DrawingTexture.filterMode = FilterMode.Point;
+        DrawingMaterial = GetComponent<Image>().material;
+        DrawingMaterial.SetTexture("_MainTex", DrawingTexture);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // calculate line
+
+            PaintCircleOnTexture(Input.mousePosition, 8, Color.red);
+        }
+    }
+
+    private void PaintCircleOnTexture(Vector3 mouseCoordinate, float radius, Color color)
+    {
+        Vector3[] corners = new Vector3[4];
+        GetComponent<RectTransform>().GetWorldCorners(corners);
+
+        float canvasWidth = corners[2].x - corners[0].x;
+        float canvasHeight = corners[2].y - corners[0].y;
+        // 0 is bottom left, 2 is top right
+
+        for (int i = 0; i < DrawingTexture.width; ++i)
+        {
+            for (int j = 0; j < DrawingTexture.height; ++j)
+            {
+                Vector3 pixelWorldPosition = new Vector3((float)i / DrawingTexture.width * canvasWidth, (float)j / DrawingTexture.height * canvasHeight, 0) + corners[0];
+
+                if (Vector3.Distance(pixelWorldPosition, mouseCoordinate) < radius)
+                {
+                    DrawingTexture.SetPixel(i, j, color);
+                }
+            }
+        }
+
+        DrawingTexture.Apply();
+        DrawingMaterial.SetTexture("_MainTex", DrawingTexture);
+    }
+
+    private void EraseCircleOnTexture(Vector3 mouseCoordinate, float radius, Color color)
+    {
+        Vector3[] corners = new Vector3[4];
+        GetComponent<RectTransform>().GetWorldCorners(corners);
+
+        float canvasWidth = corners[2].x - corners[0].x;
+        float canvasHeight = corners[2].y - corners[0].y;
+        // 0 is bottom left, 2 is top right
+
+        for (int i = 0; i < DrawingTexture.width; ++i)
+        {
+            for (int j = 0; j < DrawingTexture.height; ++j)
+            {
+                Vector3 pixelWorldPosition = new Vector3((float)i / DrawingTexture.width * canvasWidth, (float)j / DrawingTexture.height * canvasHeight, 0) + corners[0];
+
+                if (Vector3.Distance(pixelWorldPosition, mouseCoordinate) < radius)
+                {
+                    DrawingTexture.SetPixel(i, j, defaultColor);
+                }
+            }
+        }
+
+        DrawingTexture.Apply();
+        DrawingMaterial.SetTexture("_MainTex", DrawingTexture);
+    }
+
+    private void EraseAllColorsOnTexture()
+    {
+        for (int i = 0; i < DrawingTexture.width; ++i)
+        {
+            for (int j = 0; j < DrawingTexture.height; ++j)
+            {
+                DrawingTexture.SetPixel(i, j, defaultColor);
+            }
+        }
+
+        DrawingTexture.Apply();
+        DrawingMaterial.SetTexture("_MainTex", DrawingTexture);
+    }
+
+    private void LateUpdate()
+    {
     }
     #endregion
 }
