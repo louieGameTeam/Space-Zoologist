ff50c487b3c173df87fc399eab038e4e373f3987
Codey Huntting <creativecodey@gmail.com>
2021-09-10 12:28:44 -0700

    Research notes have not editable titles again

---
+++ Space-Zoologist/Assets/Scripts/Notebook/ResearchSingleNoteUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+using TMPro;
+
+public class ResearchSingleNoteUI : NotebookUIChild
+{
+    // So that it appears in the editor
+    [System.Serializable] public class StringStringEvent : UnityEvent<string, string> { }
+
+    #region Private Editor Fields
+    [SerializeField]
+    [Tooltip("Text used to display the label for this note")]
+    private TextMeshProUGUI labelText;
+    [SerializeField]
+    [Tooltip("Input field used to write the note")]
+    private TMP_InputField myInputField;
+    #endregion
+
+    #region Public Methods
+    public void Setup(ResearchCategory category, string label, ScrollRect scrollTarget)
+    {
+        // Setup the UI child base
+        base.Setup();
+
+        // Read the initial note value
+        string initialNote = UIParent.Notebook.Research.GetEntry(category).Notes.ReadNote(label);
+
+        // Setup the initial note and label
+        labelText.text = label + ":";
+        myInputField.text = initialNote;
+
+        // When input finishes editing then write the note to the notebook model
+        myInputField.onEndEdit.AddListener(s =>
+        {
+            UIParent.Notebook.Research.GetEntry(category).Notes.WriteNote(label, s);
+        });
+
+        // Make the scroll event of the input field target the scroll rect
+        OnScrollEventInterceptor interceptor = myInputField.gameObject.AddComponent<OnScrollEventInterceptor>();
+        interceptor.InterceptTarget = scrollTarget;
+    }
+    #endregion
+}
