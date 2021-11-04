1ae5094717c420c78647388d5c6c3407371b42e5
Codey Huntting <creativecodey@gmail.com>
2021-11-04 15:28:18 -0700

    Just the beginning
    Added lots of code for tutorial manager and setup some callbacks on the tutorial dialogue

---
+++ Space-Zoologist/Assets/Scripts/Managers/Dialogue/TutorialDialogueManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DialogueEditor;
+
+public class TutorialDialogueManager : MonoBehaviour
+{
+    #region Private Properties
+    private TutorialCoroutineScheduler CoroutineScheduler
+    {
+        get
+        {
+            if(!coroutineScheduler)
+            {
+                // Try to find an existing scheduler
+                coroutineScheduler = FindObjectOfType<TutorialCoroutineScheduler>();
+
+                // If no scheduler exists, then create one
+                if(!coroutineScheduler)
+                {
+                    GameObject schedulerObject = new GameObject("Tutorial Coroutine Scheduler");
+                    coroutineScheduler = schedulerObject.AddComponent<TutorialCoroutineScheduler>();
+                }
+            }
+            return coroutineScheduler;
+        }
+    }
+    #endregion
+
+    #region Private Fields
+    private TutorialCoroutineScheduler coroutineScheduler;
+    #endregion
+
+    #region Public Methods
+    public void ToggleUI(bool active)
+    {
+        GameManager.Instance.m_menuManager.ToggleUI(active);
+    }
+    public void FreezeUntilNotebookOpen()
+    {
+        CoroutineScheduler.FreezeUntilConditionIsMet(() => GameManager.Instance.NotebookUI.IsOpen);
+    }
+    public void FreezeUntilResearchTabOpen()
+    {
+        FreezeUntilNotebookTabOpen(NotebookTab.Research);
+    }
+    public void FreezeUntilGoatTerrainHighlightPresent()
+    {
+        FreezeUntilHighlightPresent(
+            new ItemID(ItemRegistry.Category.Species, 0), 
+            0,
+            new ResearchEncyclopediaArticleHighlight(195, 336));
+    }
+    public void FreezeUntilGoatFirstHighlightAbsent()
+    {
+        FreezeUntilHighlightAbsent(
+            new ItemID(ItemRegistry.Category.Species, 0), 
+            0,
+            new ResearchEncyclopediaArticleHighlight(0, 127));
+    }
+    public void FreezeUntilSlugPicked(string pickerNameFilter)
+    {
+        FreezeUntilItemPicked(NotebookTab.Research, new ItemID(ItemRegistry.Category.Species, 4), pickerNameFilter);
+    }
+    #endregion
+
+    #region Private Methods
+    private void FreezeUntilNotebookTabOpen(NotebookTab tab)
+    {
+        NotebookUI notebook = GameManager.Instance.NotebookUI;
+        CoroutineScheduler.FreezeUntilConditionIsMet(() =>
+        {
+            return notebook.IsOpen && notebook.TabPicker.CurrentTab == tab;
+        });
+    }
+    private void FreezeUntilHighlightPresent(ItemID item, int articleIndex, ResearchEncyclopediaArticleHighlight targetHighlight)
+    {
+        NotebookUI notebook = GameManager.Instance.NotebookUI;
+
+        // Get the list of all highlights in this encyclopedia article
+        List<ResearchEncyclopediaArticleHighlight> highlights = notebook
+            .Notebook.Research.GetEntry(item)
+            .Encyclopedia.GetArticle(articleIndex).Highlights;
+
+        CoroutineScheduler.FreezeUntilConditionIsMet(() =>
+        {
+            // Get index of a highlight that this highlight contains
+            int indexOfMatch = highlights.FindIndex(current => current.Contains(targetHighlight));
+            // Freeze until highlight is found and notebook is open to research tab
+            return indexOfMatch >= 0 && notebook.IsOpen && notebook.TabPicker.CurrentTab == NotebookTab.Research;
+        });
+    }
+    private void FreezeUntilHighlightAbsent(ItemID item, int articleIndex, ResearchEncyclopediaArticleHighlight targetHighlight)
+    {
+        NotebookUI notebook = GameManager.Instance.NotebookUI;
+
+        // Get the list of all highlights in this encyclopedia article
+        List<ResearchEncyclopediaArticleHighlight> highlights = notebook
+            .Notebook.Research.GetEntry(item)
+            .Encyclopedia.GetArticle(articleIndex).Highlights;
+
+        CoroutineScheduler.FreezeUntilConditionIsMet(() =>
+        {
+            // Get an index of any highlight that overlaps the target highlight
+            int indexOfMatch = highlights.FindIndex(current => targetHighlight.Overlap(current));
+            // Freeze until no overlapping highlights found and notebook is open to research tab
+            return indexOfMatch < 0 && notebook.IsOpen && notebook.TabPicker.CurrentTab == NotebookTab.Research;
+        });
+    }
+    private void FreezeUntilItemPicked(NotebookTab targetTab, ItemID targetItem, string nameFilter)
+    {
+        NotebookUI notebook = GameManager.Instance.NotebookUI;
+        // Get all pickers in the given tab
+        ItemPicker[] pickers = notebook.TabPicker.GetTabRoot(targetTab).GetComponentsInChildren<ItemPicker>(true);
+        // Find a picker whose name contains the filter
+        ItemPicker picker = Array.Find(pickers, p => p.name.IndexOf(nameFilter, StringComparison.OrdinalIgnoreCase) >= 0);
+
+        CoroutineScheduler.FreezeUntilConditionIsMet(() =>
+        {
+            return picker.SelectedItem == targetItem && notebook.IsOpen && notebook.TabPicker.CurrentTab == targetTab;
+        });
+    }
+    #endregion
+}
--- Space-Zoologist/Assets/Scripts/Notebook/ResearchEncyclopedia.cs
+++ Space-Zoologist/Assets/Scripts/Notebook/ResearchEncyclopedia.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Notebook/Encyclopedia")]
 public class ResearchEncyclopedia : ScriptableObject
 {
+    #region Public Properties
     // Public accessors
     public Dictionary<ResearchEncyclopediaArticleID, ResearchEncyclopediaArticle> Articles => articles;
+    #endregion
 
+    #region Private Editor Fields
     // Private editor data
     [SerializeField]
     [Tooltip("List of all the articles in this encyclopedia")]
     private List<ResearchEncyclopediaArticle> articlesList;
+    #endregion
 
+    #region Private Fields
     // Private data
     [Tooltip("Maps the id of the articles to the article itself for faster lookup")]
     private Dictionary<ResearchEncyclopediaArticleID, ResearchEncyclopediaArticle> articles = new Dictionary<ResearchEncyclopediaArticleID, ResearchEncyclopediaArticle>();
+    #endregion
 
+    #region Public Methods
     public void Setup()
     {
         foreach(ResearchEncyclopediaArticle article in articlesList)
@@ -27,4 +34,12 @@ public class ResearchEncyclopedia : ScriptableObject
     }
     // Get the article with the given ID
     public ResearchEncyclopediaArticle GetArticle(ResearchEncyclopediaArticleID id) => Articles[id];
+    public ResearchEncyclopediaArticle GetArticle(int index)
+    {
+        if (index >= 0 && index < articlesList.Count) return articlesList[index];
+        else throw new System.IndexOutOfRangeException($"{nameof(ResearchEncyclopedia)}: " +
+            $"attempting to get article at index {index} on encyclopedia '{name}', " +
+            $"but no such article exists");
+    }
+    #endregion
 }
