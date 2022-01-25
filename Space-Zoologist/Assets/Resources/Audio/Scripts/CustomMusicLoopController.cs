1dd0bf872151c11b9d3b1ecd798c5654e86135a5
notpresident35 <notpresident35@gmail.com>
2022-01-24 16:48:41 -0800

    Implemented workaround, still broken because of another unity bug

--- Space-Zoologist/Assets/Resources/Audio/Scripts/CustomMusicLoopController.cs
+++ Space-Zoologist/Assets/Resources/Audio/Scripts/CustomMusicLoopController.cs
@@ -13,18 +13,13 @@ public class CustomMusicLoopController : MonoBehaviour, System.IEquatable<Custom
 {
     [SerializeField] bool displayDebugInfo;
     bool hasCustomLoopData;         // whether this audio clip has special looping behavior
-    bool usingPrimarySource = true; // whether source is currently playing instead of source2
-    bool nextSourcePrepared;        // whether the next audio source has been reset
-    float loopStartTime;            // the length, in samples, of the portion of the track which is looped
     float loopEndTime;              // the time which indicates that we should loop back to the start of the loop
-    float loopPrepTime;             // the time which indicates that we should prep the next track to be looped
 
-    double startedTime;
-    float startedSourceTime;
+    // TODO: Remove after done debugging
     double scheduledEndTime;
 
-    AudioSource source;             // the audio source which contains the track to be looped
-    AudioSource source2;            // the audio source used to allow custom looped tracks to loop seamlessly
+    AudioSource source;             // the audio source which contains the intro track
+    AudioSource source2;            // the audio source which contains the main loop track
 
     public AudioSource Source => source;
     public bool isPlaying => source.isPlaying || source2.isPlaying;
@@ -53,101 +48,31 @@ public class CustomMusicLoopController : MonoBehaviour, System.IEquatable<Custom
         }
         string clipName = source.clip.name;
 
-        // the clips are named as follows: "TRACK_NAME[LOOP_START-LOOP_END-NUM_BARS].wav"
-        int openBracketIndex = clipName.IndexOf('['); // first, find the index of the '['
-        hasCustomLoopData = openBracketIndex > -1; // if we don't find '[', assume we have no loop data
+        // the clips are named as follows: "TRACK_NAME _ LOOP_INDICATOR.wav"
+        int underscoreIndex = clipName.IndexOf('_'); // first, find the index of the '_'
+        hasCustomLoopData = underscoreIndex > -1; // if we don't find '_', assume we have no loop data
 
         if (hasCustomLoopData)
         {
-            source.loop = false; // we handle looping on this script, so don't use the AudioSource's loop functionality
+            source.loop = false;
 
-            int closeBracketIndex = clipName.IndexOf(']');
-            if (closeBracketIndex == -1)
-            { // if we don't find a close bracket, 
-                Debug.LogError("Loop data is invalid: no ']' found!");
-                return;
-            }
+            // get all characters before the underscore
+            string trackTitle = clipName.Substring(0, underscoreIndex);
 
-            // start with the character after the open bracket, and before the close bracket
-            string loopData = clipName.Substring(openBracketIndex + 1, closeBracketIndex - openBracketIndex - 1);
-            string[] tokens = loopData.Split('-');
-            if (tokens.Length != 3)
-            {
-                Debug.LogError("Loop data is invalid: incorrect number of tokens found!");
-                return;
-            }
-
-            int loopStartBar = int.Parse(tokens[0]);
-            int loopEndBar = int.Parse(tokens[1]);
-            int totalBarCount = int.Parse(tokens[2]);
-
-            // the sample of the "loop end" is (loopEndBar / totalBarCount) of the way through the track
-            loopEndTime = source.clip.length * loopEndBar / totalBarCount;
-            //print (loopEndTime + ", " + source.clip.length);
-
-            // the length of the loop is (loopEnd - "loop start")
-            loopStartTime = source.clip.length * loopStartBar / totalBarCount;
-
-            loopPrepTime = loopStartTime + (loopEndTime - loopStartTime) / 2;
+            loopEndTime = source.clip.length;
 
             if (!transform.GetChild(0) || !transform.GetChild (0).GetComponent<AudioSource> ()) {
                 Debug.LogError ("This object should have a child object with an audio source!");
                 return;
             }
             source2 = transform.GetChild (0).GetComponent<AudioSource> ();
+            source2.clip = Resources.Load<AudioClip> ("Audio/Music/" + trackTitle + "_MainLoop");
+            source2.loop = true;
         }
         else
         {
             source.loop = true;
         }
-    }
-
-    void Update()
-    {
-        if (!isPlaying || !hasCustomLoopData) return;
-        
-        // One source has stopped playing and looped around, so it needs to be prepared to loop again
-        if (!source.isPlaying || !source2.isPlaying) {
-            PrepNextTrack ();
-        }
-
-        //print (source.time + ", " + source2.time);
-    }
-
-    // Tells the current track to end and next track to start at the loopEndTime
-    void PrepNextTrack() 
-    {
-        if (usingPrimarySource) 
-        {
-            // Schedules the next source to be played once the current source hits the end of the loop
-            source2.PlayScheduled (AudioSettings.dspTime + loopEndTime - source.time);
-            // Schedules the current source to end at the end of the loop
-            source2.SetScheduledEndTime (AudioSettings.dspTime + loopEndTime * 2 - source.time - loopStartTime);
-            // Skips the next source ahead to the start of the loop
-            source2.time = loopStartTime;
-
-            startedTime = AudioSettings.dspTime;
-            startedSourceTime = source.time;
-            scheduledEndTime = AudioSettings.dspTime + loopEndTime - source.time;
-
-            //print ("Prepped, will loop at " + (AudioSettings.dspTime + loopEndTime - source.time));
-        } else 
-        {
-            // Schedules the next source to be played once the current source hits the end of the loop
-            source.PlayScheduled (AudioSettings.dspTime + loopEndTime - source2.time);
-            // Schedules the current source to end at the end of the loop
-            source.SetScheduledEndTime (AudioSettings.dspTime + loopEndTime * 2 - source2.time - loopStartTime);
-            // Skips the next source ahead to the start of the loop
-            source.time = loopStartTime;
-
-            startedTime = AudioSettings.dspTime;
-            startedSourceTime = source2.time;
-            scheduledEndTime = AudioSettings.dspTime + loopEndTime - source2.time;
-
-            //print ("Prepped, will loop at " + (AudioSettings.dspTime + loopEndTime - source2.time));
-        }
-
-        usingPrimarySource = !usingPrimarySource;
     }
 
     // start playing the track
@@ -160,15 +85,12 @@ public class CustomMusicLoopController : MonoBehaviour, System.IEquatable<Custom
         }
 
         // reset state, just in case
-        source2.Stop ();
         source.time = 0;
         source2.time = 0;
-        usingPrimarySource = true;
-        nextSourcePrepared = false;
 
         source.PlayScheduled (AudioSettings.dspTime);
-        source.SetScheduledEndTime (AudioSettings.dspTime + loopEndTime);
-        //PrepNextTrack ();
+        source2.PlayScheduled (AudioSettings.dspTime + loopEndTime);
+        scheduledEndTime = loopEndTime;
 
         gameObject.name = "Now Playing: " + source.clip.name;
 
@@ -212,7 +134,7 @@ public class CustomMusicLoopController : MonoBehaviour, System.IEquatable<Custom
             return;
         }
 
-        if (usingPrimarySource)
+        if (source2.time != 0)
         {
             source2.UnPause ();
         } else
@@ -225,8 +147,9 @@ public class CustomMusicLoopController : MonoBehaviour, System.IEquatable<Custom
     // returns the realtime position of the track
     public float GetCurrentTime()
     {
-        if (usingPrimarySource) {
-            return source2.time;
+        if (source2.time != 0) {
+            // Because this loops after the intro, add the intro length to the clip time
+            return source2.time + source.clip.length;
         } else {
             return source.time;
         }
@@ -269,8 +192,7 @@ public class CustomMusicLoopController : MonoBehaviour, System.IEquatable<Custom
 
     void OnGUI () {
         if (displayDebugInfo) {
-            GUI.TextArea (new Rect (10, 10, Screen.width - 10, 40), source.time.ToString () + ", " + source2.time.ToString () + ", " + usingPrimarySource);
-            GUI.TextArea (new Rect (10, Screen.height - 40, Screen.width - 10, Screen.height - 10), startedTime + ", " + startedSourceTime + ", " + scheduledEndTime);
+            GUI.TextArea (new Rect (10, 10, Screen.width - 10, 40), source.time.ToString () + ", " + source2.time.ToString () + ", " + scheduledEndTime);
         }
     }
 }
