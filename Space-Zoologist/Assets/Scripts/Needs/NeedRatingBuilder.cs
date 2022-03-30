98fd35168808e759d5d992519f11808bb3a16774
Codey Huntting <creativecodey@gmail.com>
2022-03-30 13:15:57 -0700

    Updated food rating to match design
    Before, the food rating was not boosted by the amount towards the max but now it is so yay

--- Space-Zoologist/Assets/Scripts/Needs/NeedRatingBuilder.cs
+++ Space-Zoologist/Assets/Scripts/Needs/NeedRatingBuilder.cs
@@ -207,10 +207,25 @@ public static class NeedRatingBuilder
             float totalUsed = preferredUsed + survivableUsed;
 
             // If we used the amound we needed, then boost the rating
-            // by the amount used that is preferred
             if (totalUsed >= minNeeded)
             {
-                return 1 + ((float)preferredUsed / totalUsed);
+                int magnitude = maxUsed - minNeeded;
+
+                // Boost amounts used to boost the rating
+                float towardsMaxBoost = 0f;
+                float preferenceBoost = preferredUsed / totalUsed;
+
+                // If there is a difference between min-max used,
+                // then half the boost comes from how close to the max is used
+                // and the other half of the boost comes from how much of 
+                // the preferred item is used
+                if (magnitude > 0)
+                {
+                    towardsMaxBoost = 0.5f * (totalUsed / maxUsed);
+                    preferenceBoost *= 0.5f;
+                }
+
+                return 1 + towardsMaxBoost + preferenceBoost;
             }
             // If we did not use the amount we needed
             // then the rating is the proportion that we needed
