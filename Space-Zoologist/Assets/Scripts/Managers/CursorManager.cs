e8fe905308c7dba2002077a0421ca3204b419375
notpresident35 <notpresident35@gmail.com>
2022-04-23 10:46:01 -0700

    Cursor swap behavior implemented

---
+++ Space-Zoologist/Assets/Scripts/Managers/CursorManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D CursorIconDefault;
+    public Texture2D CursorIconInspector;
+
+    void Start()
+    {
+        EventManager.Instance.SubscribeToEvent(EventType.InspectorToggled, () => {
+                Cursor.SetCursor(((bool) EventManager.Instance.EventData ? CursorIconInspector : null), Vector2.zero, CursorMode.Auto);
+                });
+    }
+}
