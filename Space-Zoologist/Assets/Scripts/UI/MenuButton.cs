badbcb70f9e1929dc7651124b6766b6d85f78c30
notpresident35 <notpresident35@gmail.com>
2022-04-25 15:57:58 -0700

    New start menu UI implemented

---
+++ Space-Zoologist/Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;  
+ using System.Collections;  
+ using UnityEngine.EventSystems;  
+ using UnityEngine.UI;
+ using TMPro;
+ 
+ // Changes the color of some text when the button is highlighted
+ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+ 
+    public TextMeshProUGUI text;
+    public Color SelectedColor;
+
+    private Color defaultColor;
+
+    private void Start() {
+        defaultColor = text.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = SelectedColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = defaultColor;
+    }
+}
