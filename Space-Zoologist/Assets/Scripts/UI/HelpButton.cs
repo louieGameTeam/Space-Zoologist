ba23d8227a71132bbeff9b9248342306fe0270fe
notpresident35 <notpresident35@gmail.com>
2022-08-30 15:26:10 -0700

    Added help buttons for each notebook page
    3 of 5 pages use placeholder text, 2 use context-adjusted copies of the help dialogue

---
+++ Space-Zoologist/Assets/Scripts/UI/HelpButton.cs
@@ -0,0 +1,14 @@
+﻿using DialogueEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpButton : MonoBehaviour
+{
+    [SerializeField] private NPCConversation HelpDialogue = default;
+
+    public void StartConversation () {
+        GameManager.Instance.m_dialogueManager.SetNewDialogue (HelpDialogue);
+        GameManager.Instance.m_dialogueManager.StartInteractiveConversation ();
+    }
+}
