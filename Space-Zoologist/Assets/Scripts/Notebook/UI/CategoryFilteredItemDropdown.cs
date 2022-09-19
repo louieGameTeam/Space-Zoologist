0b2d80b210c6b50c1066778bfb0292abb509be3a
Draygon <Raiden152@yahoo.com>
2022-09-19 02:05:29 -0700

    Extracted functionality of controlling dropdown arrow graphic to separate script, added documentation to Google Drive under name "DropdownArrowController"

--- Space-Zoologist/Assets/Scripts/Notebook/UI/CategoryFilteredItemDropdown.cs
+++ Space-Zoologist/Assets/Scripts/Notebook/UI/CategoryFilteredItemDropdown.cs
@@ -15,11 +15,6 @@ public class CategoryFilteredItemDropdown : ItemDropdown
     [SerializeField]
     [Tooltip("Research category type that this dropdown represents")]
     protected List<ItemRegistry.Category> categoryFilter;
-    [SerializeField]
-    [Tooltip("Reference to the dropdown arrow")]
-    protected Image dropdownArrow;
-    private Vector3 collapsedRotation;  // Rotation of dropdown arrow when dropdown is collapsed
-    private Vector3 expandedRotation;   // Rotation of dropdown arrow when dropdown is expanded
     #endregion
 
     #region Public Methods
@@ -29,22 +24,6 @@ public class CategoryFilteredItemDropdown : ItemDropdown
         
         // Now that type filter is set we will setup the base class
         base.Setup();
-        if (dropdownArrow != null)
-        {
-            collapsedRotation = new Vector3(dropdownArrow.transform.eulerAngles.x, dropdownArrow.transform.eulerAngles.y, dropdownArrow.transform.eulerAngles.z);
-            expandedRotation = new Vector3(collapsedRotation.x, collapsedRotation.y, collapsedRotation.z + 180);
-        }
-    }
-    public void OnDropdownExpand()
-    {
-        ExpandDropdownArrow();
-    }
-    public void OnDropdownCollapse()
-    {
-        if (dropdownArrow.transform.eulerAngles == expandedRotation)
-        {
-            CollapseDropdownArrow();
-        }
     }
     #endregion
 
@@ -54,14 +33,6 @@ public class CategoryFilteredItemDropdown : ItemDropdown
         return base.GetItemIDs(source)
             .Where(id => categoryFilter.Contains(id.Category) && UIParent.Data.ItemIsUnlocked(id))
             .ToArray();
-    }
-    private void CollapseDropdownArrow()
-    {
-        dropdownArrow.transform.eulerAngles = collapsedRotation;
-    }
-    private void ExpandDropdownArrow()
-    {
-        dropdownArrow.transform.eulerAngles = expandedRotation;
     }
     #endregion
 }
