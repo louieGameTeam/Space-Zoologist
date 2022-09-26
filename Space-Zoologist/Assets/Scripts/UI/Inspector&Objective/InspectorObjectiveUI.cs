9f95d6681e8ee58fe5f29c0de3b70570170a954d
MSI\arthu <arthurgan29@gmail.com>
2022-09-25 23:14:38 -0700

    inspector minimizing

--- Space-Zoologist/Assets/Scripts/UI/Inspector&Objective/InspectorObjectiveUI.cs
+++ Space-Zoologist/Assets/Scripts/UI/Inspector&Objective/InspectorObjectiveUI.cs
@@ -25,14 +25,26 @@ public class InspectorObjectiveUI : MonoBehaviour
     [SerializeField]
     [Tooltip("Reference to the main inspector script")]
     private Inspector inspector = null;
+
+    [Header("Minimize/Maximize Fields")]
+    [SerializeField]
+    [Tooltip("Button that minimizes the panel")]
+    private Button minimizeButton;
+    [SerializeField]
+    [Tooltip("Button that maximizes the panel")]
+    private Button maximizeButton;
+    [SerializeField]
+    [Tooltip("Main Panel/Window, excluding maximize buttons")]
+    private CanvasGroup mainDisplayWindowCanvasGroup;
     #endregion
+
+    private bool wasInInspector = false;
 
     #region Monobehaviour Messages
     private void Start()
     {
         // Initialize the inspector
         inspector.Initialize();
-
         // Add listeners to the toggle events
         objectiveToggle.onValueChanged.AddListener(x =>
         {
@@ -44,13 +56,40 @@ public class InspectorObjectiveUI : MonoBehaviour
             if (x) inspector.OpenInspector();
             else inspector.CloseInspector();
         });
-
+        maximizeButton.onClick.AddListener(ShowInspectorObjectiveWindow);
+        minimizeButton.onClick.AddListener(HideInspectorObjectiveWindow);
+        ShowInspectorObjectiveWindow();
         // Enable the toggle and invoke the event
         objectiveToggle.isOn = true;
         inspectorToggle.onValueChanged.Invoke(true);
         inspector.CloseInspector();
+
     }
     #endregion
+
+    private void HideInspectorObjectiveWindow()
+    {
+        mainDisplayWindowCanvasGroup.alpha = 0f;
+        mainDisplayWindowCanvasGroup.blocksRaycasts = false;
+        maximizeButton.gameObject.SetActive(true);
+        if (inspector.IsInInspectorMode)
+        {
+            wasInInspector = true;
+            inspector.CloseInspector();
+        }
+        AudioManager.instance.PlayOneShot(SFXType.General);
+    }
+
+    private void ShowInspectorObjectiveWindow()
+    {
+        mainDisplayWindowCanvasGroup.alpha = 1f;
+        mainDisplayWindowCanvasGroup.blocksRaycasts = true;
+        maximizeButton.gameObject.SetActive(false);
+        if (wasInInspector)
+            inspector.OpenInspector();
+        wasInInspector = false;
+        AudioManager.instance.PlayOneShot(SFXType.General);
+    }
 
     #region Public Methods
     public void SetIsOpen(bool isOpen)
